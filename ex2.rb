9e76ea5aac488045be18a60bddb4a49d8cb81aec
Jethro <jethrodw@protonmail.com>
2020-05-17 15:41:01 +0200

    Initial commit LRTHW

---
+++ ex2.rb
@@ -0,0 +1,9 @@
+# commenting is done like this.
+
+puts "I could have code like this" # after hash.
+
+# comments can also be used to disable a string.
+
+# puts "this won't run"
+
+puts "this will run"
---
+++ ex5.rb
@@ -0,0 +1,16 @@
+my_name = 'Jethro de Wet'
+my_age = 20
+my_height = 199 # cm
+my_weight = 85 # kg's
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Let's talk about #{my_name}"
+puts "He's #{my_height} cm tall."
+puts "He's #{my_weight} kg's heavy"
+puts "Actually that's not too heavy"
+puts "He's got #{my_eyes} eyes and #{my_hair} hair"
+puts "His teeth are usually #{my_teeth} depending on the coffee"
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
